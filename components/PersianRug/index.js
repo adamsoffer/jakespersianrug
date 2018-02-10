7f15c38540271f3535a7e65b73ab92749f9f32bb
Adam Soffer <ads1018@gmail.com>
2018-02-10 02:33:39 -0500

    Style paragraph

--- components/PersianRug/index.js
+++ components/PersianRug/index.js
@@ -5,12 +5,12 @@ import Link from 'next/link'
 export default ({ price }) => (
   <Container>
     <Wrapper>
-      On February 7th, 2018,
+      On February 7th, 2018,{' '}
       <Link href="https://twitter.com/jekrb" passHref>
         <StyledLink target="_blank" rel="noopener noreferrer">
           Jake Burden
         </StyledLink>
-      </Link>
+      </Link>{' '}
       bought a persian rug for 376.344 Spank ($70 at the time).
       <Div marginTop="24px">That rug is now worth:</Div>
       <Price>${price.toFixed(2)}</Price>
