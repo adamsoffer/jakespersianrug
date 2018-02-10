9e746107a1cb5b3b31c8a7b1a6d67316c24e1653
Adam Soffer <ads1018@gmail.com>
2018-02-10 02:22:36 -0500

    Add realtime updates with streamdata

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,12 @@
 import React from 'react'
-import fetch from 'isomorphic-fetch'
 import Main from '../lib/layout'
 import PersianRug from '../components/PersianRug'
+import withPrice from '../lib/withPrice'
 
-export default class extends React.Component {
-  static async getInitialProps({ req }) {
-    const res = await fetch(
-      'https://api.coinmarketcap.com/v1/ticker/spankchain/'
-    )
-    const json = await res.json()
-    return { price: json[0].price_usd * 376.344 }
-  }
-
-  render() {
-    return (
-      <Main>
-        <PersianRug price={this.props.price} />
-      </Main>
-    )
-  }
-}
+export default withPrice(({ price }) => {
+  return (
+    <Main>
+      <PersianRug price={price} />
+    </Main>
+  )
+})
