f6992e011f75ed4d68c0f4529fca163956b4ac2d
Adam Soffer <adam.soffer@digitalsurgeons.com>
2018-02-23 16:11:54 -0500

    Update meta data

--- pages/_document.js
+++ pages/_document.js
@@ -21,6 +21,7 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <title>Jake's Persian Rug</title>
+          <meta charset="utf-8" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
@@ -29,6 +30,32 @@ export default class MyDocument extends Document {
             name="description"
             content="Real time price updates for Jake's persian rug."
           />
+          <meta
+            name="image"
+            content="https://www.jakespersianrug.com/static/jake.jpg"
+          />
+          <meta itemprop="name" content="Jakes Persian Rug" />
+          <meta
+            itemprop="description"
+            content="Real time price updates for Jake's persian rug."
+          />
+          <meta
+            itemprop="image"
+            content="https://www.jakespersianrug.com/static/jake.jpg"
+          />
+          <meta name="og:title" content="Jakes Persian Rug" />
+          <meta
+            name="og:description"
+            content="Real time price updates for Jake's persian rug."
+          />
+          <meta
+            name="og:image"
+            content="https://www.jakespersianrug.com/static/jake.jpg"
+          />
+          <meta name="og:url" content="https://www.jakespersianrug.com/" />
+          <meta name="og:site_name" content="Jake's Persian Rug" />
+          <meta name="og:type" content="website" />
+
           <style dangerouslySetInnerHTML={{ __html: this.props.css }} />
           <script
             dangerouslySetInnerHTML={{
